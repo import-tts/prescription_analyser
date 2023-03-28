1e2d6185832b81a6664b36871d5d0eb0cdc3d94d
Tautvydas <120493976+import-tts@users.noreply.github.com>
2023-03-28 09:30:05 +0300

    Update prescription_api.py

--- prescription_api.py
+++ prescription_api.py
@@ -17,18 +17,21 @@ class Perscription(BaseModel):
 
 app = FastAPI()
 
-# nurodome savo pickle'into modelio vieta kompiuteryje
+# nurodome savo pickle'into modelio vietą kompiuteryje
 with open("/Users/tautis/Downloads/model.pkl", "rb") as f:
     model = pickle.load(f)
 
+# metodas pasisveikinti su vartotoju root path'e
 @app.get('/')
 def index():
     return {'welcome_message': ''' Welcome to Perscription Analyzer's homepage '''}
 
+# metodas paaiškinti spėjamų verčių reikšmę
 @app.get('/explain_prediction')
 def index():
-    return {'explain_message': 'The possible outputs for prediction are 0, meaning the perscription is unlikely to be used, and 1, meaning the perscription is likely to be used'}
+    return {'explain_message': 'The possible outputs for prediction are 0, meaning the prescription is unlikely to be used, and 1, meaning the prescription is likely to be used'}
 
+# spėjimo metodas, pritaiko pateiktas reikšmes į pandas df ir pateikia jį modeliui
 @app.post('/prediction')
 def get_perscription_prediction(data: Perscription):
     received = data.dict()
@@ -38,5 +41,6 @@ def get_perscription_prediction(data: Perscription):
 
     return (f'Predicted value: {pred_name}')
 
+# aplikacijos paleidimas
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=4000)
